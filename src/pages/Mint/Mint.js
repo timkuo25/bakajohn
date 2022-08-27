328998f9179f292c509dc7bcd29abe6e40f2dd28
timkuo25 <b05703100@ntu.edu.tw>
2022-08-28 04:00:07 +0800

    fix width

--- src/pages/Mint/Mint.js
+++ src/pages/Mint/Mint.js
@@ -19,10 +19,10 @@ const Mint = ({ accounts, setAccounts }) => {
     return (
         <div id='mint'>
             <div className='mint-container'>
-                <img src={require('../../images/nft/11.png')} width={300}></img>
-                <img className='hidden' src={require('../../images/nft/12.png')} width={300}></img>
-                <img className='hidden' src={require('../../images/nft/13.png')} width={300}></img>
-                <img className='hidden' src={require('../../images/nft/14.png')} width={300}></img>
+                <img src={require('../../images/nft/11.png')} width={'25%'}></img>
+                <img className='hidden' src={require('../../images/nft/12.png')} width={'25%'}></img>
+                <img className='hidden' src={require('../../images/nft/13.png')} width={'25%'}></img>
+                <img className='hidden' src={require('../../images/nft/14.png')} width={'25%'}></img>
             </div>
         </div>
     );
