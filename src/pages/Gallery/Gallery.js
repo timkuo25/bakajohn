e2eb74dcbe08f71efd912a94c623d3170ff68103
timkuo25 <b05703100@ntu.edu.tw>
2022-08-28 04:09:36 +0800

    fix width again

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -4,11 +4,11 @@ const Gallery = () => {
     return (
         <div id='gallery'>
             <div className='gallery-container'>
-                <img src={require('../../images/nft/6.png')} width={200}></img>
-                <img className='hidden' src={require('../../images/nft/7.png')} width={200}></img>
-                <img className='hidden' src={require('../../images/nft/8.png')} width={200}></img>
-                <img className='hidden' src={require('../../images/nft/9.png')} width={200}></img>
-                <img className='hidden' src={require('../../images/nft/10.png')} width={200}></img>
+                <img src={require('../../images/nft/6.png')} width={'15%'}></img>
+                <img className='hidden' src={require('../../images/nft/7.png')} width={'15%'}></img>
+                <img className='hidden' src={require('../../images/nft/8.png')} width={'15%'}></img>
+                <img className='hidden' src={require('../../images/nft/9.png')} width={'15%'}></img>
+                <img className='hidden' src={require('../../images/nft/10.png')} width={'15%'}></img>
             </div>
         </div>
     );
